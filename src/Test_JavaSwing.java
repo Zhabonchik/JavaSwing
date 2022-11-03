8cf040791d5af98562417be2ca5cdcdc980a0fdb
Nikita Shapel <shapelnik@mail.ru>
2022-11-03 22:48:25 +0300

    "AbstractAction"

--- src/Test_JavaSwing.java
+++ src/Test_JavaSwing.java
@@ -5,13 +5,15 @@ import javax.swing.*;
 import javax.tools.Tool;
 
 public class Test_JavaSwing {
+    static JFrame frame = createFrame();
+    static JPanel panel = new JPanel();
     public static void main(String[] args) {
-        JFrame frame = createFrame();
-        JPanel panel = new JPanel();
         frame.add(panel,  BorderLayout.CENTER);
-        JButton button1 = new JButton("Button1");
+        /*JButton button1 = new JButton(new myAction());
+        button1.setText("button1");*/
         //WindowAdapter / WindowListener - обработка действий с окном
 
+        JButton button1 = new JButton("Button1");
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -19,8 +21,18 @@ public class Test_JavaSwing {
             }
         });
         panel.add(button1);
-        frame.pack();
+        //frame.pack();
     }
+
+    /*static class myAction extends AbstractAction {
+        myAction() {
+            putValue(AbstractAction.SHORT_DESCRIPTION, "Description");
+        }
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            panel.setBackground(Color.black);
+        }
+    }*/
 
     static JFrame createFrame() {
         JFrame frame =  new JFrame(){};
